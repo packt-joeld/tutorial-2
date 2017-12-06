372b54fae128d49d390d8de1632259241618ea37
packt-joeld <joeld@packtpub.com>
2017-12-06 15:38:09 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,9 +6,7 @@ tds = root.cssselect('td') # get all the <td tags
 for td in tds:
     record = { "td" : td.text } # column name and value
     try:
-        scraperwiki.sqlite.save(["td"], record) # save the records one by \
-one
+        scraperwiki.sqlite.save(["td"], record) # save the records one by one
     except:
         record = { "td" : "NO ENTRY" } # column name and value
-        scraperwiki.sqlite.save(["td"], record) # save the records one by \
-one
+        scraperwiki.sqlite.save(["td"], record) # save the records one by one
